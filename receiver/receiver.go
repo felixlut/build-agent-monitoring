fceb81844d58c153294fdba7c3775f4f1527f0a8
Felix Luthman <felix.luthman@gmail.com>
2022-07-31 02:53:23 +0200

    move everything

--- receiver/receiver.go
+++ receiver/receiver.go
@@ -3,8 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"time"
 
+	backoff "github.com/cenkalti/backoff/v4"
 	"github.com/go-playground/webhooks/v6/github"
+	"github.com/jackc/pgx"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -13,6 +17,32 @@ const (
 )
 
 func main() {
+	// retryAttempts := 10
+	// retryDelay := 5
+
+	log.Info("Parsing URI")
+	connConfig, err := pgx.ParseURI(os.Getenv("DATABASE_URL"))
+	if err != nil {
+		panic(err)
+	}
+
+	log.Info("Connecting to database...")
+	var conn *pgx.Conn
+	connectToDB := func() error {
+		conn, err = pgx.Connect(connConfig)
+		return err
+	}
+	err = backoff.RetryNotify(connectToDB, backoff.NewExponentialBackOff(), func(err error, duration time.Duration) {
+		log.Info(fmt.Sprintf("Error encountered, retrying in %f seconds", duration.Seconds()))
+		log.Info(err)
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+	log.Info("Connection established")
+
 	githubHook, _ := github.New(github.Options.Secret("MyGitHubSuperSecretSecrect"))
 
 	http.HandleFunc(githubPath, func(w http.ResponseWriter, r *http.Request) {
@@ -30,7 +60,7 @@ func main() {
 		case github.WorkflowJobPayload:
 			workflowJob := load
 			// Do whatever you want from here...
-			fmt.Printf("%+v\n", workflowJob)
+			log.Info("%+v\n", workflowJob)
 
 		default:
 			log.Warning(fmt.Sprintf("Payload type not handled - Type: %T", load))
