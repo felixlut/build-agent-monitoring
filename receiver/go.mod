93b6962b1df78944ce168d0c75926d07cbc0a719
Felix Luthman <felix.luthman@gmail.com>
2022-07-31 04:09:43 +0200

    move db to it's own foldr

--- receiver/go.mod
+++ receiver/go.mod
@@ -5,18 +5,23 @@ go 1.18
 require (
 	github.com/cenkalti/backoff/v4 v4.1.3
 	github.com/go-playground/webhooks/v6 v6.0.1
-	github.com/jackc/pgx v3.6.2+incompatible
+	github.com/jackc/pgx/v4 v4.16.1
 	github.com/sirupsen/logrus v1.9.0
 )
 
 require (
-	github.com/cockroachdb/apd v1.1.0 // indirect
-	github.com/gofrs/uuid v4.0.0+incompatible // indirect
-	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
-	github.com/lib/pq v1.10.2 // indirect
-	github.com/pkg/errors v0.8.1 // indirect
-	github.com/shopspring/decimal v1.2.0 // indirect
-	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	github.com/gofrs/uuid v4.2.0+incompatible // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.12.1 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.11.0 // indirect
+	github.com/lib/pq v1.10.6 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/shopspring/decimal v1.3.1 // indirect
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
+	golang.org/x/sys v0.0.0-20220730100132-1609e554cd39 // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
--- receiver/receiver.go
+++ receiver/receiver.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"os"
@@ -8,7 +9,7 @@ import (
 
 	backoff "github.com/cenkalti/backoff/v4"
 	"github.com/go-playground/webhooks/v6/github"
-	"github.com/jackc/pgx"
+	"github.com/jackc/pgx/v4"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -16,20 +17,19 @@ const (
 	githubPath = "/github"
 )
 
-func main() {
-	// retryAttempts := 10
-	// retryDelay := 5
 
-	log.Info("Parsing URI")
-	connConfig, err := pgx.ParseURI(os.Getenv("DATABASE_URL"))
-	if err != nil {
-		panic(err)
-	}
+func generateSQLSelectQuery(fields, webhookTable string) string {
+	return fmt.Sprintf("select %s from %s", fields, webhookTable)
+}
+
+func main() {
+	webhookTable := os.Getenv("WEBHOOK_TABLE")
 
 	log.Info("Connecting to database...")
 	var conn *pgx.Conn
+	var err error
 	connectToDB := func() error {
-		conn, err = pgx.Connect(connConfig)
+		conn, err = pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
 		return err
 	}
 	err = backoff.RetryNotify(connectToDB, backoff.NewExponentialBackOff(), func(err error, duration time.Duration) {
@@ -40,11 +40,10 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
-	defer conn.Close()
+	defer conn.Close(context.Background())
 	log.Info("Connection established")
 
 	githubHook, _ := github.New(github.Options.Secret("MyGitHubSuperSecretSecrect"))
-
 	http.HandleFunc(githubPath, func(w http.ResponseWriter, r *http.Request) {
 		payload, err := githubHook.Parse(r, github.WorkflowJobEvent)
 		if err != nil {
